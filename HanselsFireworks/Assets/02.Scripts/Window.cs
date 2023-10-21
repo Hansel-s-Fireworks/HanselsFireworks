d6789a8daad56ccb6bbdc4c7f4034e86e5a9e105
ericjangdev_HomeDeskTop <ericjangdev@gmail.com>
2023-10-22 03:11:12 +0900

    Add Feature
    - 마녀 베리어 = Wall
    - 냄새 적용
    - 내비 메쉬 새로 깜
    - 창문 깨는 거 시도중

---
+++ HanselsFireworks/Assets/02.Scripts/Window.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Window : InteractableObject
+{
+    [Header("Audio Clips")]
+    [SerializeField] private AudioClip audioClipBreak;
+
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    public override void TakeScore()
+    {
+        GameManager.Instance.score += this.score;
+    }
+
+    public override void TakeDamage(int damage)
+    {
+        bool isDie = DecreaseHP(damage);
+        if (isDie)
+        {
+            PlaySound(audioClipBreak);
+            gameObject.SetActive(false);                // 비활성화
+            GetComponent<BreakFruit>().Run();
+            Debug.Log("Window Breaked");
+        }
+    }
+}
